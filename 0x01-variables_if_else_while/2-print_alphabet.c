a45d142bd7147b3fa9a73647ceca356ac2b8e590
Freda Tsorme-Dzebu <fredatd2016@gmail.com>
2023-04-23 16:59:39 -0700

    Print lowercase alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/* Print the lowercase alphabets*/
+/**
+ * main - lowercase alphabets
+ * Return: 0 means Success
+ */
+int main(void)
+{
+	char th;
+
+	for (th = 'a' ; th <= 'z' ; th++)
+	{
+		putchar(th);
+	}
+	return (0);
+}
