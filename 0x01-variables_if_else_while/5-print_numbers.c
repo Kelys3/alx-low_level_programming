84e999f7277955db6eaec37e8ec3a1e41467a56e
Freda Tsorme-Dzebu <fredatd2016@gmail.com>
2023-04-23 18:41:56 -0700

    print 0to9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/* Decimal numbers from 0 to 9 */
+/**
+ * main - print numbers from 0-9
+ *Return: 0 means Success
+ */
+int main(void)
+{
+	int k;
+
+	for (k = '0' ; k <= 9 ; k++)
+	{
+		printf("%d", k);
+	}
+		printf("\n");
+		return (0);
+}
