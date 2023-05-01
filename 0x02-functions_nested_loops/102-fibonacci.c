fb268fa4849b336d5fbcb8c7b7235b695bc009f7
Freda Tsorme-Dzebu <fredatd2016@gmail.com>
2023-05-01 11:21:38 -0700

    fibo50

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/* A program that prints the first 50 Fibonacci numbers */
+/**
+ * main - prints the first 50 Fibonacci numbers
+ * Return: 0 means Success
+ */
+
+int main(void)
+{
+	int n = 1, l = 2, y, q, z = 50;
+
+	printf("%d, %d, ", n, l);
+
+	for (q = 3; q <= z; q++)
+	{
+		y = n + l;
+		printf("%d", y);
+		n = l;
+		l = y;
+		if (q != z)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
