433a17864f615675089734085b2bd6676de2e741
Freda Tsorme-Dzebu <fredatd2016@gmail.com>
2023-05-05 10:10:50 -0700

    Mozart

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/* A function that encodes a string into 1337 */
+/**
+ * *leet - encodes a string
+ * @n: the string
+ * Return: n
+ */
+char *leet(char *n)
+{
+	int i, j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (n[i] == s1[j])
+			{
+				n[i] = s2[j];
+				break;
+			}
+		}
+	}
+	return (n);
+}
