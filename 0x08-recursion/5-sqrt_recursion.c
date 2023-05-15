638da09e82de880bf51adddb605ba737f1ad31c3
Freda Tsorme-Dzebu <fredatd2016@gmail.com>
2023-05-14 18:49:19 -0700

    Sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,43 +1,38 @@
 #include "main.h"
 
-/*
- * _sqrt - calculates the midpoint of a range and returns the squareroot
- * @n: the number to be used
- * @low: a lower bound
- * @high: an upper bound
- * Return: the midpoint if it is equal to n
- */
-int _sqrt(int n, int low, int high)
-{
-	int m = (low + high) / 2;
-	int s = (m * m);
 
-	if (low > high)
-		return (-1);
-	if (s == n)
-		return (m);
-	else if (s < n)
-		return (_sqrt(n, m + 1, high));
-	else
-		return (_sqrt(n, low, m - 1));
-}
-
+int actual_sqrt_recursion(int n, int i);
 
 
 /**
- * _sqrt_recursion - returns the natural square root of a number
+ * _sqrt_recursion - returns the sqrt of a number
  * @n: the number
- * Return: the square root of n
- * otherwise return is -1
+ * Return: the sqrt
  */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	else if (n == 0)
-		return (0);
-	else if (n == 1)
-		return (1);
-	else
-		return (_sqrt(n, 0, n));
+	return (actual_sqrt_recursion(n, 0));
+}
+
+
+/**
+ * actual_sqrt_recursion - recurses to find sqrt of
+ * a number
+ * @n: the number
+ * @i: iterator
+ * Return: the sqrt
+ */
+int actual_sqrt_recursion(int n, int i)
+{
+	if (i * i > n)
+	{
+		return (-1);
+	}
+	if (i * i == n)
+	{
+		return (i);
+	}
+	return (actual_sqrt_recursion(n, i + 1));
 }
