92860975a4b82e503430ff2a248b3a41bf242cfc
Freda Tsorme-Dzebu <fredatd2016@gmail.com>
2023-05-22 15:12:23 -0700

    structdog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog's description
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: name of dog's owner
+ *
+ * Description: this header file describes a dog in details
+ * taking into account it's name, age and owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
