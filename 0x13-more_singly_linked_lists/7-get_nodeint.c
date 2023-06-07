7667f47f6c5543faa0edb1532aebc579d86364cc
Freda Tsorme-Dzebu <fredatd2016@gmail.com>
2023-06-06 19:13:17 -0700

    added task7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,12 +10,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
 
-	if (head != NULL)
+	for (i = 0; i < index && head != NULL; i++)
 	{
-		for (i = 0; i < index; i++)
-		{
-			head = head->next;
-		}
+		head = head->next;
 	}
 	return (head);
 }
