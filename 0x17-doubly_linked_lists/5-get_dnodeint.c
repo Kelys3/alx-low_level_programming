9015d9b981f5a4d4a2839ed4d95bd4e88b95f364
Freda Tsorme-Dzebu <fredatd2016@gmail.com>
2023-07-20 20:01:59 -0700

    betty

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -4,7 +4,7 @@
  * get_dnodeint_at_index - finds a node in a dlistint_t list.
  * @head: pointer to the head of the dlistint_t list.
  * @index: index of the node, starting from 0
- * Return: the nth node. Otherwise NULL if node does not exist 
+ * Return: the nth node. Otherwise NULL if node does not exist
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
