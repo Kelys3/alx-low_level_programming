ee85fbb755d65f2428ff8f600e44be31be141548
Freda Tsorme-Dzebu <fredatd2016@gmail.com>
2023-07-24 19:02:08 -0700

    help me win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Kelys3/alx-low_level_programming/raw/master/0x18-dynamic_libraries/file.so
+wget -P /tmp https://github.com/Freda Tsorme-Dzebu/alx-low_level_programming/raw/master/0x18-dynamic_libraries/file.so
 export LD_PRELOAD=/tmp/file.so
